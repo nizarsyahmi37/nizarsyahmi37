4eb9980812fe2190bc6d676070c44ff01a5757ef
Nizar Syahmi bin Nazlan <38692430+nizarsyahmi37@users.noreply.github.com>
2025-07-23 15:20:47 +0000

    Refactor code structure for improved readability and maintainability

---
+++ app/todo/page.tsx
@@ -0,0 +1,6 @@
+import { getData } from "@/actions/todoAction";
+import Todos from "@/components/todos/todos";
+export default async function Home() {
+  const data = await getData();
+  return <Todos todos={data} />;
+}
